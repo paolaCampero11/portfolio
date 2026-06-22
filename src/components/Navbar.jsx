0e777aca183ad2cb90b325e7c7c881e9f44b2d24
paolaCampero11 <paolacampero32@gmail.com>
2026-06-22 18:46:44 -0400

    fix: style: add shadow to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-background shadow-md z-50">
+    <nav className="fixed top-0 left-0 w-full bg-background shadow-md shadow-secondary/20 z-50 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           
